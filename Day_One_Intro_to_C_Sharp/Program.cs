1a367193c8264bf9f9f006914fd0a960f65651cb
ChrisE1984 <chris.estrada1984@proton.me>
2026-09-08 15:21:03 -0700

    First Update with More Code

--- Day_One_Intro_to_C_Sharp/Program.cs
+++ Day_One_Intro_to_C_Sharp/Program.cs
@@ -1,6 +1,6 @@
 ﻿//With the double forward slash I am able to write a comment in my code. This is a single line comment.
 
-//WriteLine looks at our console and prints whatever is in the paraenthesis. The text must be in quotes or it will not work.
+//WriteLine looks at our console and prints whatever is in the parentheses. The text must be in quotes or it will not work.
 //WriteLines are considered Outputs.
 
 //Console.Clear() clears the console of any text that was previously printed to it. This is useful if you want to start fresh with a new message or output.
@@ -16,5 +16,27 @@ Console.WriteLine(5);
 //"" shows a String (or text) data type. Strings are used to represent text in programming languages, and can include letters, numbers, symbols, and spaces. They are typically enclosed in double quotes to distinguish them from other data types like integers or booleans.
 // 5 is an integer (whole number) data type. Integers are used to represent whole numbers in programming languages, and can be positive, negative, or zero. They are typically used for counting, indexing, and performing mathematical operations.
 
-Console.Write("Lets test this out!");
+Console.WriteLine("Lets test this out!");
 //Console.Write does not have a line break after the output, so any subsequent output will be printed on the same line and joined. This can be useful for creating prompts or messages that require user input, or for formatting output in a specific way.
+
+//Variables are used to store data in programming languages. They can hold different types of data, such as numbers, text, or boolean values. Variables are typically declared with a name and a data type, and can be assigned a value that can be changed later in the program. They are useful for storing and manipulating data in a program, and can help make code more readable and maintainable.
+//In C# variables have explicit data types, meaning that the type of data a variable can hold must be specified when it is declared. This helps prevent errors and makes the code easier to read and understand. C# also supports implicit typing, where the type of a variable is inferred from the value assigned to it, but this is less common and can lead to confusion if not used carefully.
+
+string firstName = "Chris";
+Console.WriteLine(firstName);
+
+//Readline allows the user to input text within the console, code will wait for user input
+
+Console.Clear ();
+
+//Console.ReadLine ();
+
+// string is changed to input then input is output, so not useful but shows you can change an output to an input
+
+Console.WriteLine("What is your favorite album?");
+
+string myfavAlbum = Console.ReadLine ();
+
+//Concatenation - the joining of two or more string (need to add space at end of string if creating a sentence)
+
+Console.WriteLine("Oh wow I love " + myfavAlbum);
