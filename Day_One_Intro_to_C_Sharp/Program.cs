64465bf88d07788232d9915321a528cda58d27cd
ChrisE1984 <chris.estrada1984@proton.me>
2026-09-08 14:33:39 -0700

    First Commit for FIrst Day

---
+++ Day_One_Intro_to_C_Sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿//With the double forward slash I am able to write a comment in my code. This is a single line comment.
+
+//WriteLine looks at our console and prints whatever is in the paraenthesis. The text must be in quotes or it will not work.
+//WriteLines are considered Outputs.
+
+//Console.Clear() clears the console of any text that was previously printed to it. This is useful if you want to start fresh with a new message or output.
+Console.WriteLine("Stay Fresh, Cheese Bag!");
+
+
+//Syntax is the set of rules that define how code should be written in a programming language. It includes things like the use of semicolons, brackets, and keywords. Following proper syntax is important for ensuring that your code runs correctly and is easy to read and understand.
+//Syntax errors occur when the code does not follow the rules of the programming language, such as missing a semicolon or using an incorrect keyword. These errors prevent the code from compiling and running correctly, and must be fixed before the program can be executed.
+Console.WriteLine("I want to eat some Chipotle!");
+
+Console.WriteLine(5);
+
+//"" shows a String (or text) data type. Strings are used to represent text in programming languages, and can include letters, numbers, symbols, and spaces. They are typically enclosed in double quotes to distinguish them from other data types like integers or booleans.
+// 5 is an integer (whole number) data type. Integers are used to represent whole numbers in programming languages, and can be positive, negative, or zero. They are typically used for counting, indexing, and performing mathematical operations.
+
+Console.Write("Lets test this out!");
+//Console.Write does not have a line break after the output, so any subsequent output will be printed on the same line and joined. This can be useful for creating prompts or messages that require user input, or for formatting output in a specific way.
