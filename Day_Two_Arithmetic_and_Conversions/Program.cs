3b9cb5b217b70960a14c3aebcf26594c47682a63
ChrisE1984 <chris.estrada1984@proton.me>
2026-09-09 10:09:25 -0700

    first commit for Arithmetic

---
+++ Day_Two_Arithmetic_and_Conversions/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+//This will be our intro to Arithmetic and Assignment Operators / Conversion Lecture
+
+
+// We are able to store sums within a variable
+// creating a variable and assigning value is Initializing it
+int num = 4+4;
+Console.WriteLine (num);
+
+Console.WriteLine("Enter your Lucky Number:");
+string luckyNumber = Console.ReadLine();
+Console.WriteLine("And add your Crush's Lucky Number:");
+string crushLuckyNumber = Console.ReadLine();
+
+// because these are strings the plus sign concatenates instead of adding them creating a logic error
+Console.WriteLine(luckyNumber + crushLuckyNumber);
+
+
+// We will use the Convert class to access our INT32 and convert the string into an integer
+int actualLuckyNum = Convert.ToInt32 (luckyNumber);
+
+int actualCrushLuckyNum = Convert.ToInt32 (crushLuckyNumber);
+
+Console.WriteLine(actualLuckyNum + actualCrushLuckyNum);
